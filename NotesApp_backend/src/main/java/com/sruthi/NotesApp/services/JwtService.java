9417ca03d9755a4ff8aacdf91d46877de1350a52
Sruthi Mora <sruthimora02@gmail.com>
2025-05-14 22:22:01 +0530

    Implement change password endpoint for authenticated users
    - Added /api/auth/change-password endpoint
    - Created ChangePasswordRequest DTO to handle request payload
    - Validates old password before allowing password update
    - Encodes and saves the new password securely

--- NotesApp_backend/src/main/java/com/sruthi/NotesApp/services/JwtService.java
+++ NotesApp_backend/src/main/java/com/sruthi/NotesApp/services/JwtService.java
@@ -26,7 +26,7 @@ public class JwtService {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 60 * 1000))   // 1 min
+                .setExpiration(new Date(System.currentTimeMillis() + 60 *  60 *1000))   // 60 min
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
